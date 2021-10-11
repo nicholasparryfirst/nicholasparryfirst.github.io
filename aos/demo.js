38e7881c8a47feb6367bdb26602b09b5504b3d96
nicholasparryfirst <87668913+nicholasparryfirst@users.noreply.github.com>
2021-10-11 10:00:26 -0400

    Eptied - New Class Start

--- aos/demo.js
+++
@@ -1,7 +0,0 @@
-function yourFavorite() {
-  var v = document.getElementById("selectField").value;
-  console.log(v);
-  document.getElementById("readFavorite").innerHTML = v;
-};
-
-console.log("demo.js loaded");
--- jQuery/selectActive.js
+++
@@ -1,4 +0,0 @@
-$('select').change(function(){
-    $(#button2).find(':active').addClass('active')
-           .siblings('option').removeClass('active');
-});
