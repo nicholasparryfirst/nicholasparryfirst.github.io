7bf3d707602fbb1af3e980f0e1ea2b9a1ef48251
nicholasparryfirst <87668913+nicholasparryfirst@users.noreply.github.com>
2021-11-08 11:19:48 -0500

    grocery2

--- pages/groceries/groceries.js
+++ pages/groceries/groceries.js
@@ -1,9 +1,15 @@
 var grocery_item = document.getElementsByClassName('groceries');
 
-function myFunction(x) {
-  if (document.getElementById(x).checked) {
-    document.getElementById('item' + x).classList.add("checked");
-  } else  {
-    console.log("heh");
+var checkbox = document.querySelector("input[name=groceries]");
+
+console.log("groceries.js loaded");
+
+checkbox.addEventListener('change', function() {
+  for (i = 0; i < checkbox.length; i++) {
+    if (checkbox[i].checked) {
+      grocery_item[i].classList.add("strike");
+    } else  {
+      grocery_item[i].classList.remove("strike");
+    }
   }
-}
+})
