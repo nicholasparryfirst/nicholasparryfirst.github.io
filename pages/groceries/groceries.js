ca0c33117027c37b5fa6f3817237ed03cd35040e
nicholasparryfirst <87668913+nicholasparryfirst@users.noreply.github.com>
2021-11-08 11:02:54 -0500

    added grocery js

--- pages/groceries/groceries.js
+++ pages/groceries/groceries.js
@@ -1,1 +1,9 @@
 var grocery_item = document.getElementsByClassName('groceries');
+
+function myFunction(x) {
+  if (document.getElementById(x).checked) {
+    document.getElementById('item' + x).classList.add("checked");
+  } else  {
+    console.log("heh");
+  }
+}
