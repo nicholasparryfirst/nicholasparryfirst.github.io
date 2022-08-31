02ab04813998cffad74b7c65cf289e302f6cf531
nicholasparryfirst <87668913+nicholasparryfirst@users.noreply.github.com>
2022-08-31 14:46:54 -0400

    the foxtail commit

---
+++ web/portfolio_v0-1/js/countdown.js
@@ -0,0 +1,30 @@
+console.log("Countdown JS Loaded");
+
+// Set the date we're counting down to
+var countDownDate = new Date("Nov 28, 2022 13:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="date_element"
+  document.getElementById("date_element").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("date_element").innerHTML = "EXPIRED";
+  }
+}, 1000);
